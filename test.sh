f6a2a24634fe9b36183284bb484ef73479ff96d0
main-horse <187762902+main-horse@users.noreply.github.com>
2025-01-17 16:52:49 +0800

    test script

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+L=8
+D=512
+bs=2
+lr=0.0003
+compile=False
+dataset=fake
+
+[ -z "$HF_HUB_TOKEN" ] && echo "HF_HUB_TOKEN is not set" && exit 1
+[ -z "$HF_HOME" ] && echo "warning: HF_HOME is not set"
+export HF_HUB_USE_HF_TRANSFER=1
+export WANDB_MODE=offline
+
+# FileNotFoundError: [Errno 2] No such file or directory: './cache/train/continuous/train/index.json' -> './cache/train/index.json'
+. .venv/bin/activate
+OMP_NUM_THREADS=32 torchrun --nproc_per_node=8 train.py \
+    --batch_size $bs \
+    --run_name lr${lr}_width512 \
+    --num_epochs 100 \
+    --learning_rate ${lr} \
+    --max_steps 5004 \
+    --evaluate_every 500 \
+    --model_width $D \
+    --model_depth $L \
+    --model_head_dim 128 \
+    --optimizer_type mup_adam \
+    --lr_scheduler_type linear \
+    --project_name openvid-diffusion-ci \
+    --dataset $dataset \
+    --compile_models $compile
